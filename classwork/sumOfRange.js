be5a30a522dba47f469a5b7ec2f38ece2a35b952
Noelia Martinez <noeliamtzrojo@gmail.com>
2021-02-04 16:24:36 +0000

    completed 'Sum Of Range' code-along

---
+++ classwork/sumOfRange.js
@@ -0,0 +1,16 @@
+// Code for challenge activity
+
+var testArray = new Array();
+var sum = 0;
+for(let i = 0; i < 5; i++) {
+    var newElement = prompt("Enter a number of your choice:");
+
+    newElement = parseInt(newElement);
+    testArray.push(newElement);
+}
+
+for (var position = 0; position < testArray.length; position++) {
+    sum += testArray[position];
+}
+
+console.log("The sum of " + testArray + " is: " + sum);
